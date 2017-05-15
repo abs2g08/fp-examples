8bfbc1cc59067f7a80ac0383132964cfbeed8374
Adam Smith <adam.smith@aerian.com>
2017-05-15 11:50:55 +0100

    changing functions to arrow functions

--- test/reverseWords.spec.js
+++ test/reverseWords.spec.js
@@ -1,26 +1,26 @@
 import reverseWords, { splitWords, reverse, joinWords } from '../src/reverseWords'
 import { expect } from 'chai'
 
-describe('reverseWords', function() {
-  it('should split string into array based on delimiter', function() {
+describe('reverseWords', () => {
+  it('should split string into array based on delimiter', () => {
     const str = 'what is this backwards?';
     const res = splitWords(str, ' ');
     expect(res).to.deep.equal(['what', 'is', 'this', 'backwards?']);
   });
 
-  it('should reverse an array', function() {
+  it('should reverse an array', () => {
     const str = 'what';
     const res = reverse(str.split('')).join('');
     expect(res).to.equal('tahw');
   });
 
-  it('should turn an array of strings to one string', function() {
+  it('should turn an array of strings to one string', () => {
     const arr = ['what', 'is'];
     const res = joinWords(arr);
     expect(res).to.equal('what is');
   });
 
-  it('should reverse "what is this backwards?"', function() {
+  it('should reverse "what is this backwards?"', () => {
     const str = 'what is this backwards?';
     const res = reverseWords(str);
     expect(res).to.equal('backwards? this is what');
--- test/wordCount.spec.js
+++ test/wordCount.spec.js
@@ -2,25 +2,25 @@ import wordCount, { lower, splitWords, remove } from '../src/wordCount'
 import { expect } from 'chai'
 
 describe('wordCount', function() {
-  it('should transform string to lower case', function() {
+  it('should transform string to lower case', () => {
     const str = 'TEST';
     const res = lower(str);
     expect(res).to.deep.equal('test');
   });
 
-  it('should split string into array based on delimiter', function() {
+  it('should split string into array based on delimiter', () => {
     const str = 'what is this backwards?';
     const res = splitWords(str, ' ');
     expect(res).to.deep.equal(['what', 'is', 'this', 'backwards?']);
   });
 
-  it('should remove specific chars from string', function() {
+  it('should remove specific chars from string', () => {
     const str = 'test! me, now this?';
     const res = remove(str, ['!', '?', ',']);
     expect(res).to.equal('test me now this');
   });
 
-  it('should count the total number of unique words in a string', function() {
+  it('should count the total number of unique words in a string', () => {
     const str = 'how many tests are, in this test! string test?';
     const res = wordCount(str);
     expect(res).to.deep.equal({
